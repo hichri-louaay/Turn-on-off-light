cb2ba1247fedf06c6b4f2cae8a40b437dcfbce63
hichri-louaay <59854427+hichri-louaay@users.noreply.github.com>
2020-09-02 19:52:58 +0100

    Add files via upload

---
+++ turn off , turn on light/index.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".on").addEventListener("click",function(){
+    document.querySelector(".img").setAttribute("src", "images/on.png");
+});
+document.querySelector(".off").addEventListener("click",function(){
+    document.querySelector("img").setAttribute("src", "images/off.png");
+});
+
